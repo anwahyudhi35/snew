d06b07e5fd881f36ca845c4ee8f2b59e9106e2de
Alvian Nur Wahyudhi <anwahyudhi35@gmail.com>
2019-10-04 04:39:08 +0800

    pak rumusnya di menu awal, ganti dari menu awal

--- aplikasi/admin/kerja/tahani/rumus_clean.php
+++ aplikasi/admin/kerja/tahani/rumus_clean.php
@@ -2,8 +2,18 @@
 
 $sql = mysqli_query($dbh, "SELECT *,DATE_FORMAT(tgl_lahir,'%d %M %Y') as lahir,TIMESTAMPDIFF(MONTH, tgl_lahir,now()) as umur FROM bayi where TIMESTAMPDIFF(MONTH, tgl_lahir,now()) < 7" ) or die(mysqli_error($dbh));
 
-while ($data = mysqli_fetch_array($sql)) {
-    
+switch ($data[kelamin]) {
+    case 'Pria':
+        print("anak anda lelaki");
+        break;
+
+    case 'Perempuan':
+        print("anak anda perempuan");
+        break;
+
+    default:
+        print("tidak terdapat data anak");
+        break;
 }
 
 ?>
